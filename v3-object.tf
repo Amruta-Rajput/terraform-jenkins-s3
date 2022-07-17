9b72e4b6a436698b855588c512978cd6e3b9e512
Amruta-Rajput <102989207+Amruta-Rajput@users.noreply.github.com>
2022-07-17 15:23:37 +0530

    Update v3-object.tf

--- v3-object.tf
+++ v3-object.tf
@@ -2,6 +2,6 @@
 resource "aws_s3_bucket_object" "example" {
   key        = "index.html"
   bucket     = aws_s3_bucket.sheruuuuu.id
-  source     = "index.html"
+  source     = "gameoflife.war"
   
 }
